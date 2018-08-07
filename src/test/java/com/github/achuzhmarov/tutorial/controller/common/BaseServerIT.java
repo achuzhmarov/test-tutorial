83ca5ef25a653b23cd9b0f9dd1f7cecc3b4b76cb
achuzhmarov <achuzhmarov@users.noreply.github.com>
2018-08-07 10:41:20 +0300

    change database clearing in full server test from @Before to @After

--- src/test/java/com/github/achuzhmarov/tutorial/controller/common/BaseServerIT.java
+++ src/test/java/com/github/achuzhmarov/tutorial/controller/common/BaseServerIT.java
@@ -2,7 +2,7 @@ package com.github.achuzhmarov.tutorial.controller.common;
 
 import com.github.achuzhmarov.tutorial.jpa.CustomerRepository;
 import com.github.achuzhmarov.tutorial.jpa.ProductRepository;
-import org.junit.Before;
+import org.junit.After;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -42,7 +42,7 @@ public abstract class BaseServerIT {
         });
     }
 
-    @Before
+    @After
     public void clearDataFromDatabase() {
         runInTransaction(() -> {
             customerRepository.deleteAll();
